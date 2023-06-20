cfb2dfad48eac60e79f817776fa6309cd00c2561
Matheus Cabral <matheuscabraldesousa@gmail.com>
2023-06-20 15:22:22 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Code for learning about StreamWriter
---
+++ Udemy.CSharp.Aula198/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Udemy.CSharp.Aula198
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\mathe\OneDrive\Documentos\Projetos C#\Udemy.CSharp.Aula198\file1.txt";
+            string targetPath = @"C:\Users\mathe\OneDrive\Documentos\Projetos C#\Udemy.CSharp.Aula198\file2.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            } catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
